9d0dd9c40511bd317ca9996aa0ddac16e715a535
Minerbotics <frcteam4827@gmail.com>
2026-02-18 19:52:48 -0500

    Updates config for motors and adds functions for control

--- subsystems/fuel_subsystem.py
+++ subsystems/fuel_subsystem.py
@@ -8,17 +8,31 @@ class FuelSubsystem(commands2.Subsystem):
     def __init__(self) -> None:
         super().__init__()
 
-        self.intakeLauncherRoller = phoenix6.hardware.TalonFX(FuelConstants._launcher_id)
-        self.feederRoller = phoenix6.hardware.TalonFX(FuelConstants._feeder_id)
-        motorConfig = phoenix6.configs.TalonFXConfiguration()
-        motorConfig.Commutation.MotorArrangement = phoenix6.signals.MotorArrangementValue.Minion_JST
-        self.intakeLauncherRoller.getConfiguration().apply(motorConfig)
-        self.feederRoller.getConfiguration().apply(motorConfig)
+        self.launcherRoller = phoenix6.hardware.TalonFXS(FuelConstants._launcher_id)
+        self.feederRoller = phoenix6.hardware.TalonFXS(FuelConstants._feeder_id)
+        motorArrangement = phoenix6.signals.MotorArrangementValue.NEO_JST
+        self.arrangementConfig = phoenix6.configs.CommutationConfigs().with_motor_arrangement(motorArrangement)
+        launcher_configurator = self.launcherRoller.configurator
+        feeder_configurator = self.feederRoller.configurator
+        launcher_configurator.apply(self.arrangementConfig)
+        feeder_configurator.apply(self.arrangementConfig)
 
     def intake(self) -> None:
-        self.feederRoller.set(0.1)
-        self.intakeLauncherRoller.set(0.1)
+        self.feederRoller.set(-0.5)
+        self.launcherRoller.set(-0.3)
+
+    def eject(self) -> None:
+        self.feederRoller.set(0.5)
+        self.launcherRoller.set(0.3)
+
+    def launch(self) -> None:
+        self.feederRoller.set(-0.3)
+        self.launcherRoller.set(-0.7)
+
+    def spinUp(self) -> None:
+        self.feederRoller.set(-0.25)
+        self.launcherRoller.set(-0.7)
 
     def stop(self) -> None:
         self.feederRoller.set(0)
-        self.intakeLauncherRoller.set(0)
+        self.launcherRoller.set(0)
