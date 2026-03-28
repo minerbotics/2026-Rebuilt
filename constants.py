fc8e20842aba7ecb7859db8033b9b203ea41e003
Minerbotics <frcteam4827@gmail.com>
2026-03-28 10:39:00 -0400

    updates from comp

--- constants.py
+++ constants.py
@@ -5,10 +5,10 @@ class FuelConstants:
     _launcher_current_limit = 60
     _feeder_voltage = 9.0
     _launcher_voltage = 9.0
-    _feeder_intake_speed = 0.5
+    _feeder_intake_speed = 1.0
     _launcher_intake_speed = 0.3
     _feeder_launch_speed = 1.0
-    _launcher_launch_speed = 0.8
+    _launcher_launch_speed = 1.0
     _feeder_spinup_speed = 0
 
 class OperatorConstants:
