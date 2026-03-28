85612a33c461a6ac8a8ecc11e5d41dacea961de5
Michael Kinnunen <10411319+Becapa@users.noreply.github.com>
2026-03-28 11:48:45 -0400

    Add vortex motors for launcher

--- constants.py
+++ constants.py
@@ -1,13 +1,14 @@
 class FuelConstants:
     _feeder_id = 10
-    _launcher_id = 11
+    _intake_id = 11
+    _left_launcher_id = 13
+    _right_launcher_id = 14
     _feeder_current_limit = 60
-    _launcher_current_limit = 60
-    _feeder_voltage = 9.0
-    _launcher_voltage = 9.0
+    _intake_current_limit = 60
     _feeder_intake_speed = 1.0
-    _launcher_intake_speed = 0.3
+    _intake_intake_speed = 0.3
     _feeder_launch_speed = 1.0
+    _intake_launch_speed = 1.0
     _launcher_launch_speed = 1.0
     _feeder_spinup_speed = 0
 
--- subsystems/fuel_subsystem.py
+++ subsystems/fuel_subsystem.py
@@ -1,6 +1,7 @@
 import commands2
 import wpilib
 import phoenix6
+import rev
 
 from constants import FuelConstants
 
@@ -8,34 +9,49 @@ class FuelSubsystem(commands2.Subsystem):
     def __init__(self) -> None:
         super().__init__()
 
-        self.launcherRoller = phoenix6.hardware.TalonFXS(FuelConstants._launcher_id)
+        self.intakeRoller = phoenix6.hardware.TalonFXS(FuelConstants._intake_id)
         self.feederRoller = phoenix6.hardware.TalonFXS(FuelConstants._feeder_id)
         motorArrangement = phoenix6.signals.MotorArrangementValue.NEO_JST
         self.arrangementConfig = phoenix6.configs.CommutationConfigs().with_motor_arrangement(motorArrangement)
-        launcher_configurator = self.launcherRoller.configurator
+        launcher_configurator = self.intakeRoller.configurator
         feeder_configurator = self.feederRoller.configurator
         launcher_configurator.apply(self.arrangementConfig)
         feeder_configurator.apply(self.arrangementConfig)
 
+        self.leftLauncher = rev.SparkFlex(FuelConstants._left_launcher_id, rev.SparkFlex.MotorType.kBrushless)
+        self.rightLauncher = rev.SparkFlex(FuelConstants._right_launcher_id, rev.SparkFlex.MotorType.kBrushless)
+        
+        follower_config = rev.SparkBaseConfig()
+        follower_config.follow(self.leftLauncher, True)
+        self.rightLauncher.configure(
+            follower_config, 
+            rev.SparkBase.ResetMode.kResetSafeParameters,
+            rev.SparkBase.PersistMode.kPersistParameters
+        )
+
+
     def intake(self) -> None:
         self.feederRoller.set(-1 * FuelConstants._feeder_intake_speed)
-        self.launcherRoller.set(-1 * FuelConstants._launcher_intake_speed)
+        self.intakeRoller.set(-1 * FuelConstants._intake_intake_speed)
 
     def eject(self) -> None:
         self.feederRoller.set(FuelConstants._feeder_intake_speed)
-        self.launcherRoller.set(FuelConstants._launcher_intake_speed)
+        self.intakeRoller.set(FuelConstants._intake_intake_speed)
 
     def launch(self) -> None:
         self.feederRoller.set(FuelConstants._feeder_launch_speed)
-        self.launcherRoller.set(-1 * FuelConstants._launcher_launch_speed)
+        self.intakeRoller.set(-1 * FuelConstants._intake_launch_speed)
+        self.leftLauncher.set(FuelConstants._launcher_launch_speed)
 
     def spinUp(self) -> None:
         self.feederRoller.set(FuelConstants._feeder_spinup_speed)
-        self.launcherRoller.set(-1 * FuelConstants._launcher_launch_speed)
+        self.intakeRoller.set(-1 * FuelConstants._intake_launch_speed)
+        self.leftLauncher.set(FuelConstants._launcher_launch_speed)
 
     def stop(self) -> None:
         self.feederRoller.set(0)
-        self.launcherRoller.set(0)
+        self.intakeRoller.set(0)
+        self.leftLauncher.set(0)
     
     def spinUpCommand(self) -> commands2.Command:
         return self.run(self.spinUp)
