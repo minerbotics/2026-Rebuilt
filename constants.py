18a49132f2b751dd92d3275f4585f7540218b914
Michael Kinnunen <10411319+Becapa@users.noreply.github.com>
2026-04-02 17:05:09 -0400

    Add pid for shooting

--- constants.py
+++ constants.py
@@ -5,12 +5,19 @@ class FuelConstants:
     _right_launcher_id = 14
     _feeder_current_limit = 60
     _intake_current_limit = 60
-    _feeder_intake_speed = 1.0
-    _intake_intake_speed = 0.3
-    _feeder_launch_speed = 1.0
-    _intake_launch_speed = 1.0
+    _feeder_intake_speed = 0.8
+    _intake_intake_speed = 0.4
+    _feeder_launch_speed = 0.8
+    _intake_launch_speed = 0.6
     _launcher_launch_speed = 1.0
     _feeder_spinup_speed = 0
+
+    _launcher_target_rpm = 5000 # example; set to tuned RPM
+    _launcher_kP = 0.0004
+    _launcher_kI = 0.0
+    _launcher_kD = 0.0
+    _launcher_rpm_tolerance = 150 # RPM tolerance for "ready"
+    _launcher_feed_when_ready = True # boolean if you want feeders gated by readiness
 
 class OperatorConstants:
     _driver_controller = 0
